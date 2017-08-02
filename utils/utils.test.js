1d77027a67b78bef2aba557b190f637437e7e968
hometownzero <bradycpeters@gmail.com>
2017-08-02 16:39:37 -0500

    now using assertion library expect to run tests instead of manually throwing errors.

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,17 +1,22 @@
 const utils = require('./utils');
+const expect = require('expect');
 
 it('Should add two numbers', () => {
     var result = utils.add(33, 11);
 
-    if(result != 44){
-        throw new Error(`Expected 44, but got ${result}`);
-    }
+    expect(result).toBe(44, `Expected 44, but got ${result}`).toBeA('number');
+
+    // if(result !== 44){
+    //     throw new Error(`Expected 44, but got ${result}`);
+    // }
 });
 
 it('Should square a number', () => {
     var result = utils.square(12);
 
-    if(result != 144){
-        throw new Error(`Expected 144, but got ${result}`);
-    }
+    expect(result).toBe(144, `Expected 144, but got ${result}`).toBeA('number');
+
+    // if(result !== 144){
+    //     throw new Error(`Expected 144, but got ${result}`);
+    // }
 });
