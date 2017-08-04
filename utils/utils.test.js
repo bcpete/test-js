f57da9b584fae4746b30e36333af314db941989a
hometownzero <bradycpeters@gmail.com>
2017-08-03 23:26:55 -0500

    refactored testing code into their own seperate blocks to make things more readable form the terminal

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,36 +1,48 @@
 const utils = require('./utils');
 const expect = require('expect');
 
-it('Should add two numbers', () => {
-    var result = utils.add(33, 11);
+describe('Utils', () => {
+  
+  describe('#add', () => {
+    
+    it('Should add two numbers', () => {
+      var result = utils.add(33, 11);
 
-    expect(result).toBe(44, `Expected 44, but got ${result}`).toBeA('number');
+      expect(result).toBe(44, `Expected 44, but got ${result}`).toBeA('number');
 
-    // if(result !== 44){
-    //     throw new Error(`Expected 44, but got ${result}`);
-    // }
-});
+      // if(result !== 44){
+      //     throw new Error(`Expected 44, but got ${result}`);
+      // }
+    });  
 
-it('Should square a number', () => {
-    var result = utils.square(12);
+    it('Should async add two numbers', (done) => {
+      utils.asyncAdd(4,3, (sum) => {
+        expect(sum).toBe(7).toBeA('number');
+        done();
+      });
+    });
+    
+  });
 
-    expect(result).toBe(144, `Expected 144, but got ${result}`).toBeA('number');
+  describe('#square', () => {
+    
+    it('Should square a number', () => {
+      var result = utils.square(12);
+
+      expect(result).toBe(144, `Expected 144, but got ${result}`).toBeA('number');
 
     // if(result !== 144){
     //     throw new Error(`Expected 144, but got ${result}`);
     // }
-});
+    });
 
-it('Should async add two numbers', (done) => {
-  utils.asyncAdd(4,3, (sum) => {
-    expect(sum).toBe(7).toBeA('number');
-    done();
-  })
-});
-
-it('Should async square a number', (done) => {
-  utils.asyncSquare(9, (sum) => {
-    expect(sum).toBe(81).toBeA('number');
-    done();
+    it('Should async square a number', (done) => {
+      utils.asyncSquare(9, (sum) => {
+        expect(sum).toBe(81).toBeA('number');
+        done();
+      });
+    });
   });
+
 });
+
